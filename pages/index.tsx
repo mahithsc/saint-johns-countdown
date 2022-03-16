1121b8f79542862932ebdcaec633310085b5edc4
mahithsc <mahith.chitrapu@gmail.com>
2022-03-16 12:33:56 -0400

    changing the UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,35 @@
+import { url } from 'inspector'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { EffectCallback, useEffect, useState } from 'react'
 
 const Home: NextPage = () => {
+
+  const [timeLeft, setTimeLeft] = useState()
+
+  useEffect(() => {
+    
+  })
+
   return (
-    <div className='flex items-center'>
-      <h1>Saint Johns Class of 2022 Countdown</h1>
-    </div>
+    <>
+      <Head>
+        <title>Saing Johns Class of 22 Countdown</title>
+      </Head>
+      <div className='flex h-screen'>
+        {/* <div className='flex'></div> */}
+        {/* <div className='text-white spacing tracking-[1em] text-3xl font-bold flex'>COUNTDOWN</div> */}
+        <div className='flex-1 bg-slate-500 flex justify-center items-center'>
+          <div className='text-white spacing tracking-[0.75em] text-3xl font-bold flex'>COUNTDOWN</div>
+        </div>
+        <div className='flex-1 bg-red-500'>
+
+        </div>
+      </div>
+    </>
   )
 }
 
 export default Home
+
