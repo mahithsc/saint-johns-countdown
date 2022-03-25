89462a283e8b147091c3366ccc9c3b5004b23f99
mahithsc <mahith.chitrapu@gmail.com>
2022-03-25 11:49:52 -0400

    cosmetic change, removing "countdown"

--- components/Title.tsx
+++ components/Title.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Title = () => {
     return (
         <>
-            <div className='text-white spacing tracking-[0.5em] text-3xl font-bold flex'>COUNTDOWN</div>
+            {/* <div className='text-white spacing tracking-[0.5em] text-3xl font-bold flex'>COUNTDOWN</div> */}
             <div className='text-white spacing tracking-[0.5em] text-3xl font-bold flex'>SJ CLASS OF '22</div>
         </>
     )
